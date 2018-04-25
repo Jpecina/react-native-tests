90ef1f4a9c17326f70f627d5356a94d502096a99
Jpecina <31221531+Jpecina@users.noreply.github.com>
2018-04-24 19:41:38 -0500

    added axios library

--- src/Components/AlbumListComponent.js
+++ src/Components/AlbumListComponent.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, ScrollView } from "react-native";
+import axios from "axios";
 
-const AlbumListComponent = () => {
-  return (
-    <View>
-      <Text>Album List!!</Text>
-    </View>
-  );
-};
+class AlbumListComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      albums: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
+  }
+  render() {
+    return (
+      <View>
+        <Text>Album List!!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumListComponent;
